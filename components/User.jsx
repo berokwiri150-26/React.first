cadc5728f6738c0613f82a57bd67a538f35174e5
berokwiri150-26 <berokwiri150@gmail.com>
2026-04-29 16:00:17 +0300

    Reviewed files, ready to be evaluated.

--- components/User.jsx
+++ components/User.jsx
@@ -14,30 +14,37 @@ const UserHobbies = (props) => {
         'Reading',
         'Networking',
     ]);
-    return (
+    return(
         <div className="hobbies-container">
-         <h2>{props.name}'s Hobbies</h2>
+         <h2>{props.name || "User"}'s Hobbies</h2>
+<p>Greetings dear viewer! Welcome to this very appealing website. It's one of a kind, I must say.</p>
+<p> Here is a list of my hobbies, so you can get to know me better. Are you ready? ;)</p>
+
             <ul>
-  {props.hobbies.map((hobby, index) => {
+  {hobbies.map((hobby, index) => {
     const specialHobbies = ['Cooking', 'Reading', 'Networking'];
     const isSpecial = specialHobbies.includes(hobby);
 
     const hobbyStyle = {
       padding: '10px',
       backgroundColor: isSpecial ? 'darkblue' : 'transparent',
-      color: isSpecial ? 'white' : 'black',
-      fontWeight: isSpecial ? 'bold' : 'normal',
-      border: isSpecial ? '3px solid darkblue' : 'none',
-      display: 'inline-block',
+      color: isSpecial ? 'black' : 'purple',
+      fontWeight: isSpecial ?  'bold': 'normal',
+      border: isSpecial ? '3px solid darkblue' : '3px liquid purple', 
+      borderRadius: '30px',
       width: '200px',
+      textAlign: isSpecial ? 'center' : 'left',
+      /*Text alignment isn't working for me, need help on that*/
     };
     return (
-      <li key={index} style={hobbyStyle}>
-        {isSpecial ? `⭐ ${hobby} ⭐` : hobby}
-      </li>
-    );
-  })}
-  </ul>
+            <li key={index} style={hobbyStyle}>
+              {isSpecial ? ` ${hobby} ` : hobby}
+            </li>
+                 );
+              })}
+           </ul>
+
+           <p> I hope you enjoyed this list of hobbies. If you have any questions, feel free to ask! </p>
         </div>
        );
     }
