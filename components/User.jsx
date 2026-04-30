a3506050accc33c8fc321425aebdb9a4632017f6
berokwiri150-26 <berokwiri150@gmail.com>
2026-04-30 19:47:55 +0300

    FInished styling the profile image, completed with work, apologies for lateness.

--- components/User.jsx
+++ components/User.jsx
@@ -1,6 +1,10 @@
 import App from '../App';
 import React, { useState } from 'react';
 
+
+  
+
+
 const UserHobbies = (props) => {
     const [hobbies] = useState ([
         'Coding',
@@ -16,6 +20,15 @@ const UserHobbies = (props) => {
     ]);
     return(
         <div className="hobbies-container">
+
+
+         <img 
+                src="https://plus.unsplash.com/premium_photo-1669725687221-6fe12c2da6b1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                className="profile-photo" 
+                alt="profile photo" 
+            />
+
+
          <h2>{props.name || "User"}'s Hobbies</h2>
 <p>Greetings dear viewer! Welcome to this very appealing website. It's one of a kind, I must say.</p>
 <p> Here is a list of my hobbies, so you can get to know me better. Are you ready? ;)</p>
@@ -35,6 +48,9 @@ const UserHobbies = (props) => {
       width: '200px',
       textAlign: isSpecial ? 'center' : 'left',
       /*Text alignment isn't working for me, need help on that*/
+
+      display: 'block',
+      textAlign: isSpecial? 'center' : 'left',
     };
     return (
             <li key={index} style={hobbyStyle}>
